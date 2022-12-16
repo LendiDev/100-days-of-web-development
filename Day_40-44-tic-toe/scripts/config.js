c792effb5333a1c5cc0a85616de9d85214b39f57
Nikita Lendiak <lendiak93@gmail.com>
2022-12-16 21:33:05 +0000

    Tic Tac Toe completed

--- Day_40-44-tic-toe/scripts/config.js
+++ Day_40-44-tic-toe/scripts/config.js
@@ -1,12 +1,31 @@
 const openPlayerConfig = (event) => {
   editedPlayer = +event.target.dataset.playerid;
 
+  if (players[editedPlayer - 1].playerName === '-') {
+    playerNameInputElement.value = "";
+  } else {
+    playerNameInputElement.value = players[editedPlayer - 1].playerName;
+  }
+
   showWithOpacity(playerConfigOverlayElement, 500);
+  showWithOpacity(backdropElement, 500);
+}
+
+const openPlayerResetWarning = (event) => {
+  editedPlayer = +event.target.dataset.playerid;
+
+  if (currentRound === 1) {
+    resetPlayerConfig();
+    return;
+  }
+
+  showWithOpacity(playerResetOverlayElement, 500);
   showWithOpacity(backdropElement, 500);
 }
 
 const closePlayerConfig = (event) => {
   hideWithOpacity(playerConfigOverlayElement, 350);
+  hideWithOpacity(playerResetOverlayElement, 350);
   hideWithOpacity(backdropElement, 350).then(() => {
     configErrorsElement.innerHTML = "";
     formElement.classList.remove('error');
@@ -37,9 +56,9 @@ const validatePlayerName = (enteredPlayerName) => {
     isValid = false;
     configErrorsElement.innerHTML += "<li>Player name must be at least 2 character long</li>";
   }
-  if (enteredPlayerName.length > 15) {
+  if (enteredPlayerName.length > 20) {
     isValid = false;
-    configErrorsElement.innerHTML += "<li>Player name must be shorter than 15 characters</li>";
+    configErrorsElement.innerHTML += "<li>Player name must be shorter than 20 characters</li>";
   }
 
   return isValid;
@@ -60,13 +79,45 @@ const savePlayerConfig = (event) => {
   }
 }
 
+const resetPlayerConfig = (event) => {
+  if (event) {
+    event.preventDefault();
+  }
+
+  hideWithOpacity(gamePlayElement, 300);
+  updatePlayer('-');
+}
+
 const updatePlayer = (playerName) => {
   const updatedPlayerDataElement = document.getElementById(`player-${editedPlayer}-data`);
   updatedPlayerDataElement.children[1].textContent = playerName;
 
   players[editedPlayer - 1].playerName = playerName;
 
-  console.log(players);
-
   closePlayerConfig();
+  savePlayersState();
+}
+
+const savePlayersState = () => {
+  window.localStorage.setItem('players', JSON.stringify(players));
+}
+
+const getPlayersState = () => {
+  return window.localStorage.getItem('players');
+}
+
+const initConfig = () => {
+
+  if (window.localStorage.getItem('players')) {
+    players = JSON.parse(window.localStorage.getItem('players'));
+  } else {
+    savePlayersState();
+  }
+
+  const player1DataElement = document.getElementById(`player-1-data`);
+  const player2DataElement = document.getElementById(`player-2-data`);
+  player1DataElement.children[1].textContent = players[0].playerName;
+  player2DataElement.children[1].textContent = players[1].playerName;
+
+  savePlayersState();
 }
