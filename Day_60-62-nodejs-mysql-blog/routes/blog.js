f451ba01753de8c3929efa10edb8aaf79d156c6e
Nikita Lendiak <lendiak93@gmail.com>
2022-12-19 23:27:49 +0000

    Added new post functianality and posts display

--- Day_60-62-nodejs-mysql-blog/routes/blog.js
+++ Day_60-62-nodejs-mysql-blog/routes/blog.js
@@ -7,13 +7,39 @@ router.get('/', (req, res) => {
   res.redirect('/posts');
 });
 
-router.get('/posts', (req, res) => {
-  res.render('posts-list');
+router.get('/posts', async (req, res) => {
+  const query = `
+    SELECT posts.*, authors.name AS author_name, authors.email FROM posts 
+    INNER JOIN authors WHERE posts.author_id = authors.id
+  `;
+
+  try {
+    const [posts] = await db.query(query);
+    res.render('posts-list', { posts } );
+  } catch (e) {
+    res.render('500');
+  }
 })
 
 router.get('/new-post', async (req, res) => {
   const [authors] = await db.query('SELECT * FROM authors');
   res.render('create-post', { authors });
+})
+
+router.post('/posts', async (req, res) => {
+  const requestData = [
+    req.body.title,
+    req.body.summary,
+    req.body.content,
+    req.body.author
+  ];
+  try {
+    await db.query('INSERT INTO posts (title, summary, body, author_id) VALUES (?)', [requestData]);
+    res.redirect('/posts');
+  } catch (e) {
+    console.log(e);
+    res.render('500');
+  }
 })
 
 module.exports = router;
