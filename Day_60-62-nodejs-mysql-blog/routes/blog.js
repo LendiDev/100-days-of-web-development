d51a69e96e71e4b36dec7720f51c98de88224fbf
Nikita Lendiak <lendiak93@gmail.com>
2022-12-19 23:53:55 +0000

    added view full blog posts

--- Day_60-62-nodejs-mysql-blog/routes/blog.js
+++ Day_60-62-nodejs-mysql-blog/routes/blog.js
@@ -7,6 +7,11 @@ router.get('/', (req, res) => {
   res.redirect('/posts');
 });
 
+router.get('/new-post', async (req, res) => {
+  const [authors] = await db.query('SELECT * FROM authors');
+  res.render('create-post', { authors });
+});
+
 router.get('/posts', async (req, res) => {
   const query = `
     SELECT posts.*, authors.name AS author_name, authors.email FROM posts 
@@ -19,12 +24,23 @@ router.get('/posts', async (req, res) => {
   } catch (e) {
     res.render('500');
   }
-})
+});
 
-router.get('/new-post', async (req, res) => {
-  const [authors] = await db.query('SELECT * FROM authors');
-  res.render('create-post', { authors });
-})
+router.get('/posts/:post_id', async (req, res) => {
+  const postId = req.params.post_id;
+  const query = `
+    SELECT posts.*, authors.name AS author_name, authors.email FROM posts 
+    INNER JOIN authors 
+    WHERE posts.author_id = authors.id AND posts.id = ${postId}
+  `;
+
+  try {
+    const [post] = await db.query(query);
+    res.render('post-detail', { post: post[0] } );
+  } catch (e) {
+    res.render('500');
+  }
+});
 
 router.post('/posts', async (req, res) => {
   const requestData = [
