10d01f3fb32c265b47816751ef211c10fe5fc96c
Nikita Lendiak <lendiak93@gmail.com>
2022-12-28 23:01:56 +0000

    Add login and registration post request connected

--- Day_71-73-authorization-register-nodejs-mongodb/routes/demo.js
+++ Day_71-73-authorization-register-nodejs-mongodb/routes/demo.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const bcryptjs = require('bcryptjs');
 
 const db = require('../data/database');
 
@@ -16,14 +17,75 @@ router.get('/login', function (req, res) {
   res.render('login');
 });
 
-router.post('/signup', async function (req, res) {});
+router.post('/signup', async function (req, res) {
+  // TODO: handle errors;
+  // TODO: handle confirmation of email is equal to actual email entered
+  const enteredEmail = req.body.email;
+  const enteredConfirmationEmail = req.body['confirm-email'];
+  const enteredPassword = req.body.password;
 
-router.post('/login', async function (req, res) {});
+  try {
+    const userExists = await db.getDb().collection('users').findOne({ email: enteredEmail });
+
+    if (userExists) {
+      console.log('user already exists');
+      return res.redirect('/login');
+    }
+  } catch (e) {
+    console.log(e);
+    return res.redirect('/register');
+  }
+
+  const encryptedPassword = await bcryptjs.hashSync(enteredPassword, 12);
+
+  const newUser = {
+    email: enteredEmail,
+    password: encryptedPassword,
+  }
+
+  try {
+    // successfully logged in
+    const response = await db.getDb().collection('users').insertOne(newUser);
+
+    if (response) {
+      return res.redirect('/login');
+    }
+    // something went wrong... the user details wasn't saved in database;
+    return res.redirect('/register');
+
+  } catch (e) {
+    console.log(e);
+    return res.redirect('/register');
+  }
+});
+
+router.post('/login', async function (req, res) {
+  // TODO: handle errors;
+  const enteredEmail = req.body.email;
+  const enteredPassword = req.body.password;
+
+  const user = await db.getDb().collection('users').findOne({ email: enteredEmail });
+
+  if (!user) {
+    console.log("User doesn't exist");
+    return res.redirect('/login');
+  }
+
+  const isPasswordMatching = await bcryptjs.compare(enteredPassword, user.password);
+
+  if (!isPasswordMatching) {
+    console.log("Password not matching the one in database");
+    return res.redirect('/login');
+  }
+
+  // successful login
+  res.redirect('/admin');
+});
 
 router.get('/admin', function (req, res) {
   res.render('admin');
 });
 
-router.post('/logout', function (req, res) {});
+router.post('/logout', function (req, res) { });
 
 module.exports = router;
